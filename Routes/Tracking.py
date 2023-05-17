c9718e457090a39e412b4c5b9abeef2a9e26a2a5
GitRitas <git@ritascorp.com>
2023-05-17 12:16:45 -0400

    restructure

--- Routes/Tracking.py
+++ Routes/Tracking.py
@@ -10,16 +10,18 @@ from datetime import date
 def trackingSheet(dpt):
     form = POForm()
     ItemForm =  POItemForm()
-    # user_dept = current_user.Department
-    # user_dept_class = Department.query.filter_by(Name=user_dept).first()
-    form.department.data = 'IT'
-    form.dept_code.data = '20'
-    form.requester.data = f"Dharmik Patel"
+    user_dept = current_user.Department
+    user_dept_class = Department.query.filter_by(Name=user_dept).first()
+    form.requester.data = current_user.Name
+    form.department.data = user_dept_class.Name
+    form.dept_code.data = user_dept_class.Dept_Code
+    # form.po_number.data = POEntry.query.order_by(POEntry.PO_Number.desc()).first() + 1
+    form.PO_number.data = '1647'
     form.order_date.data = f"{date.today()}"
     if request.method == 'POST':
         po_number = 1655
         entry = POEntry(
-            PO_Number = po_number,
+            PO_Number = form.PO_number.data,
             Dept_Code = form.dept_code.data,
             Department = form.department.data,
             Description = form.description.data,
@@ -42,7 +44,7 @@ def trackingSheet(dpt):
         item_count = int(request.form['item_count'])
         for i in range(1, item_count+1):
             item = POItem(
-                PO_Number = po_number,
+                PO_Number = form.PO_number.data,
                 Product_Name = request.form[f'product_name_{i}'],
                 Quantity = int(request.form[f'quantity_{i}']),
                 Price = float(request.form[f'price_{i}'])
@@ -51,6 +53,6 @@ def trackingSheet(dpt):
             
         db.session.add_all(items)
         db.session.commit()
-        return redirect(url_for('trackingSheet', dpt=dpt))        
+        return redirect(url_for('Tracking/trackingSheet', dpt=dpt))        
     entries = POEntry.query.filter_by(Department=dpt)
-    return render_template("trackingSheet.html", entries=entries, form=form, itemform=ItemForm, date=date)
+    return render_template("Tracking/trackingSheet.html", entries=entries, form=form, itemform=ItemForm, date=date)
